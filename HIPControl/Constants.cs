772612ee1f97e4e7977bf265e55b425a85bfde44
Joe Voisin <jvoisin@gmail.com>
2014-02-07 00:46:03 -0500

    Feb 7th Test Build

--- HIPControl/Constants.cs
+++ HIPControl/Constants.cs
@@ -12,15 +12,13 @@ namespace HIPControl
         internal const string HIPStopParams = "/stop epolicy3.5"; //change 'epolicy3.5' to actual password not the test password.
         internal const string HIPStartParams = "/start";
 
-        //internal const string PingHosts = "DC01,DC02,DRDC01,DRDC02";
-        //internal const string PingHosts = "10.128.33.177,10.128.33.170,10.116.10.10,10.116.10.11";
-        internal const string PingHosts = "1.2.3.4";
+        internal const string PingHosts = "10.128.33.1,10.128.34.1,10.116.10.1";
 
         internal const string TestURL = "http://www.meridiancu.ca"; //url to test
-        internal const string URLContentCheckRegEx = "mMeridian Credit Union"; //use any regex
+        internal const string URLContentCheckRegEx = "<title>Meridian Credit Union</title>"; //use any regex
         internal const int TestURLTimeout = 5000; //milliseconds for web site to load
 
-        internal const int TimeOut = 1; //firewall re-enable seconds
-        internal const int RecheckInterval = 20; //seconds between rechecking for AD servers and webserver.
+        internal const int TimeOut = 5; //firewall re-enable minutes
+        internal const int RecheckInterval = 20; //seconds between rechecking for ICMP and webserver.
     }
 }
